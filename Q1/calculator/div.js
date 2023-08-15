cdc845716a1dc4cceff6ba9b9a7900e2504cb9e6
MBilalZarnab <bilnabkhan@gmail.com>
2023-08-15 21:27:30 +0500

    CALCULTOR

---
+++ Q1/calculator/div.js
@@ -0,0 +1,4 @@
+export default function divkaro(num1, num2) {
+    let result = num1 / num2;
+    return result;
+}
---
+++ Q1/calculator/sub.ts
@@ -0,0 +1,6 @@
+export default function subkaro(num1:number, num2:number){
+    let result= num1 -  num2;
+
+    return result;
+
+}
